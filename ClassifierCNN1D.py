a8ba958dae5602b84146f98c8bc71a4c1bea7675
mashichao <hzmashichao@163.com>
2019-03-16 16:37:29 +0000

    wish me luck

---
+++ ClassifierCNN1D.py
@@ -0,0 +1,102 @@
+#Shichao.Ma$$$IndividualProject
+#This is the Conventional Neural Network
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import Utility
+import random as rn
+
+
+#To avoid the randomness
+import os
+os.environ['PYTHONHASHSEED']=str(1)
+np.random.seed(1)
+rn.seed(1)
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+from keras import backend as K
+tf.set_random_seed(1)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+
+
+
+
+
+class ClassifierCNN1D:
+
+    data_training = []
+    label_training = []
+    data_testing = []
+
+    predictions = []
+
+    init_weights = keras.initializers.glorot_normal(seed=1);
+    regularizer = keras.regularizers.l2(l=0.00001)
+
+
+
+    def __init__(self, data_training, label_training, data_testing):
+        self.data_training = np.array(data_training).reshape(len(data_training), 3000, 1)
+        self.label_training = [Utility.label_str2array(l) for l in label_training]
+        self.label_training = np.array(self.label_training).reshape(len(label_training), 15)
+
+        self.data_testing = data_testing
+
+
+
+
+    def train_the_model(self):
+        model = keras.models.Sequential()
+        # TODO more convolutional layers, extract more useful features
+        model.add(keras.layers.Conv1D(filters=20, kernel_size=50, activation='tanh', input_shape=(3000, 1), padding='same'))
+        model.add(keras.layers.MaxPooling1D(pool_size=5))
+        #model.add(keras.layers.Dropout(0.3))
+        model.add(keras.layers.Conv1D(filters=20, kernel_size=30, activation='tanh', kernel_initializer=self.init_weights, padding='same'))
+        model.add(keras.layers.MaxPooling1D(pool_size=3))
+        #model.add(keras.layers.Dropout(0.3))
+        #model.add(keras.layers.Conv1D(filters=10, kernel_size=20, activation='tanh', kernel_initializer=self.init_weights, padding='same'))
+        #model.add(keras.layers.MaxPooling1D(pool_size=2))
+        #model.add(keras.layers.Dropout(0.3))
+        model.add(keras.layers.Flatten())
+        model.add(keras.layers.Dense(120, activation='tanh'))
+        model.add(keras.layers.Dropout(0.5))
+        model.add(keras.layers.Dense(15, activation=tf.nn.softmax))
+        opt = keras.optimizers.Adam(lr=0.005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+        model.compile(loss='mean_squared_error',
+                      optimizer=opt,
+                      metrics=['accuracy'])
+        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=5, verbose=0,
+                                                       mode='auto', baseline=None)
+        model.fit(self.data_training,
+                  self.label_training,
+                  epochs=50,
+                  batch_size=50,
+                  shuffle=True,
+                  callbacks=[early_stopping])
+
+
+
+
+
+
+
+        # Do the prediction
+        for d_t in self.data_testing:
+            d_t = np.array(d_t).reshape(len(d_t), 3000, 1)
+            prediction = model.predict(d_t)
+            prediction = [Utility.label_num2str(np.argmax(p)) for p in prediction]
+            self.predictions.append(max(set(prediction), key=prediction.count))
+
+
+    #To get the prediction through the model
+    def get_predictions(self):
+        return self.predictions
+
+
+
+
+
+
+
+
---
+++ Utility.py
@@ -0,0 +1,133 @@
+import pywt
+import numpy as np
+#from statsmodels.tsa.ar_model import AR
+from scipy.stats import skew
+import os
+import csv
+from tensorflow import keras
+import re
+
+
+
+
+#$$$$ functions for data handling
+def read_csv(f):
+    matrix = []
+    #TODO crop???
+    i = 0
+    with f as csvFile:
+        reader = csv.reader(csvFile)
+        for row in reader:
+            row = [float(i) for i in row]
+            matrix.append(row)
+            i = i + 1
+            if i >= 70000:
+                break
+    csvFile.close()
+    return matrix
+
+
+
+
+#TODO another collection function/ collect data per channel!
+def collect_data_with_windowing_per_channel(directory, data, labels, subject, index):
+    for filename in os.listdir(directory):
+        for s in subject:
+            for i in index:
+                x = re.findall(s + "_..." + i + "_" + "\d\d", filename)
+                if x:
+                    os.chdir(directory)
+                    f = open(filename)
+                    matrix = np.array(read_csv(f))
+                    label = os.path.splitext(filename)[0][3:6]
+                    for i in range(8):
+                        data.append(matrix[i].flatten())
+                        labels.append(label)
+
+def collect_testing_data_with_windowing_per_channel(directory, data, labels, subject, index):
+    for s in subject:
+        for m in ["HC_", "I_I", "I_M", "IMR", "L_L", "M_M", "M_R", "MRL", "R_L", "R_R", "T_I", "T_L", "T_M", "T_R", "T_T"]:
+            for i in index:
+                combination_matrix = []
+                for filename in os.listdir(directory):
+                    x = re.findall(s + "_" + m + i + "_" + "\d\d", filename)
+                    if x:
+                        os.chdir(directory)
+                        f = open(filename)
+                        matrix = np.array(read_csv(f))
+                        for y in range(8):
+                            combination_matrix.append(matrix[y].flatten())
+                data.append(combination_matrix)
+                labels.append(m)
+
+
+
+
+
+
+#$$$$ functions for classification
+def get_accuracy(predictions, labels_test):
+    accuracy = 0
+    for i in range(len(labels_test)):
+        if predictions[i] == labels_test[i]:
+            accuracy = accuracy + 1
+    return accuracy/len(labels_test)
+
+def label_str2num(label):
+    return {
+        'HC_': 0,
+        'I_I': 1,
+        'I_M': 2,
+        'IMR': 3,
+        'L_L': 4,
+        'M_M': 5,
+        'M_R': 6,
+        'MRL': 7,
+        'R_L': 8,
+        'R_R': 9,
+        'T_I': 10,
+        'T_L': 11,
+        'T_M': 12,
+        'T_R': 13,
+        'T_T': 14,
+    }[label]
+
+def label_str2array(label):
+    return {
+        'HC_': [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        'I_I': [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        'I_M': [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        'IMR': [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        'L_L': [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        'M_M': [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        'M_R': [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]],
+        'MRL': [[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]],
+        'R_L': [[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]],
+        'R_R': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]],
+        'T_I': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]],
+        'T_L': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]],
+        'T_M': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]],
+        'T_R': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]],
+        'T_T': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]],
+    }[label]
+
+def label_num2str(label):
+    return {
+        0: 'HC_',
+        1: 'I_I',
+        2: 'I_M',
+        3: 'IMR',
+        4: 'L_L',
+        5: 'M_M',
+        6: 'M_R',
+        7: 'MRL',
+        8: 'R_L',
+        9: 'R_R',
+        10: 'T_I',
+        11: 'T_L',
+        12: 'T_M',
+        13: 'T_R',
+        14: 'T_T',
+    }[label]
+
+
