0f51ac3de08658be89ff52902b8815213a0a90ff
Shichao Ma <hzmashichao@163.com>
2019-03-18 23:29:26 +0000

    Update ClassifierCNN1D.py

--- ClassifierCNN1D.py
+++ ClassifierCNN1D.py
@@ -71,7 +71,7 @@ class ClassifierCNN1D:
                                                        mode='auto', baseline=None)
         model.fit(self.data_training,
                   self.label_training,
-                  epochs=800
+                  epochs=800,
                   batch_size=30,
                   shuffle=True,
                   callbacks=[early_stopping])
