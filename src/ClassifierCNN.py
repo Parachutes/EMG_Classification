98d544c495e67f204f690b1ff2bfbed0aa41dc77
Shichao Ma <hzmashichao@163.com>
2019-03-21 16:03:52 +0000

    Update ClassifierCNN.py

--- src/ClassifierCNN.py
+++ src/ClassifierCNN.py
@@ -37,13 +37,13 @@ class ClassifierCNN:
     def train_the_model(self):
         model = keras.models.Sequential()
         # TODO more convolutional layers, extract more useful features
-        model.add(keras.layers.Conv2D(filters=5, kernel_size=(8,50), activation='tanh', input_shape=self.data_training[0].shape, padding='same', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
+        model.add(keras.layers.Conv2D(filters=50, kernel_size=(8,50), activation='tanh', input_shape=self.data_training[0].shape, padding='same', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
         model.add(keras.layers.MaxPooling2D(pool_size=(2,4)))
         model.add(keras.layers.Dropout(0.2))
-        model.add(keras.layers.Conv2D(filters=5, kernel_size=(4,20), activation='tanh', padding='same', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
+        model.add(keras.layers.Conv2D(filters=30, kernel_size=(4,20), activation='tanh', padding='same', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
         model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
         model.add(keras.layers.Dropout(0.2))
-        model.add(keras.layers.Conv2D(filters=5, kernel_size=(2,10), activation='tanh', padding='same', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
+        model.add(keras.layers.Conv2D(filters=20, kernel_size=(2,10), activation='tanh', padding='same', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
         model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
         model.add(keras.layers.Dropout(0.5))
         model.add(keras.layers.Flatten())
