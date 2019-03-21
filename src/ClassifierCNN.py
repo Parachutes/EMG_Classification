f9cbcb58131d28b3c2d47dfefd967f9959151eda
Shichao Ma <hzmashichao@163.com>
2019-03-21 21:31:49 +0000

    Update ClassifierCNN.py

--- src/ClassifierCNN.py
+++ src/ClassifierCNN.py
@@ -55,12 +55,12 @@ class ClassifierCNN:
         model.compile(loss='mean_squared_error',
                       optimizer=opt,
                       metrics=['accuracy'])
-        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=10, verbose=0,
+        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=3, verbose=0,
                                                        mode='auto', baseline=None)
         model.fit(self.data_training,
                   self.label_training,
                   epochs=800,
-                  batch_size=20,
+                  batch_size=40,
                   shuffle=True,
                   callbacks=[early_stopping])
 
