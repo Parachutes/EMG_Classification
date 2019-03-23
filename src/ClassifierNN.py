000cca53640f78d7ee768fbff64402c5a1b7f337
Shichao Ma <hzmashichao@163.com>
2019-03-23 19:11:08 +0000

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -49,13 +49,13 @@ class ClassifierNN:
         model.add(keras.layers.Dropout(0.3))
         model.add(keras.layers.Dense(150, activation='tanh',
                                      kernel_regularizer = self.regularizer))
-        model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dropout(0.3))
         model.add(keras.layers.Dense(150, activation='tanh',
                                      kernel_regularizer = self.regularizer))
-        model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dropout(0.3))
         model.add(keras.layers.Dense(100, activation='tanh',
                                      kernel_regularizer = self.regularizer))
-        model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dropout(0.3))
         model.add(keras.layers.Dense(15, activation=tf.nn.softmax,
                                      kernel_regularizer = self.regularizer))
         #Optimizers
@@ -63,7 +63,7 @@ class ClassifierNN:
         model.compile(loss='mean_squared_error',
                       optimizer=opt,
                       metrics=['accuracy'])
-        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=10, verbose=0, mode='auto', baseline=None)
+        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=20, verbose=0, mode='auto', baseline=None)
         model.fit(self.data_training, self.label_training,
                   epochs=2000,
                   batch_size=30,
