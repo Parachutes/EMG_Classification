32fcf4a4f998af70ecd719f029c5e2bf3a5952b2
Shichao Ma <hzmashichao@163.com>
2019-03-23 16:45:07 +0000

    Update ClassifierSVM.py

--- src/ClassifierSVM.py
+++ src/ClassifierSVM.py
@@ -21,7 +21,7 @@ class ClassifierSVM:
 
 
     def get_predictions(self):
-        clf = svm.SVC(C = 10, gamma = 0.18)
+        clf = svm.SVC(C = 8, gamma = 0.15)
         clf.fit(self.data_training, self.label_training)
         for d_t in self.data_testing:
             d_t = [d.flatten() for d in d_t]
