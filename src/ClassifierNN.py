2999ae93d026a518aa1f7e2290ebaa347161effb
Shichao Ma <hzmashichao@163.com>
2019-03-23 22:57:23 +0000

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -59,6 +59,9 @@ class ClassifierNN:
         model.add(keras.layers.Dense(180, activation='relu',
                                      kernel_regularizer = self.regularizer))
         model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dense(180, activation='relu',
+                                     kernel_regularizer = self.regularizer))
+        model.add(keras.layers.Dropout(0.4))
         model.add(keras.layers.Dense(15, activation=tf.nn.softmax,
                                      kernel_regularizer = self.regularizer))
         #Optimizers
