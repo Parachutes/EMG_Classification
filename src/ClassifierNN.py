434e50aa202fe7cceaf960fcd4562ca9c32d6a86
Shichao Ma <hzmashichao@163.com>
2019-03-23 19:35:56 +0000

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -44,12 +44,12 @@ class ClassifierNN:
     def train_the_model(self):
         model = keras.models.Sequential()
         #The layers of NN
-        model.add(keras.layers.Dense(200, activation='relu', input_dim=self.input_size,
+        model.add(keras.layers.Dense(180, activation='relu', input_dim=self.input_size,
                                      kernel_regularizer = self.regularizer))
         model.add(keras.layers.Dropout(0.4))
-        model.add(keras.layers.Dense(200, activation='relu',
+        model.add(keras.layers.Dense(180, activation='relu',
                                      kernel_regularizer = self.regularizer))
-        model.add(keras.layers.Dropout(0.5))
+        model.add(keras.layers.Dropout(0.4))
         model.add(keras.layers.Dense(15, activation=tf.nn.softmax,
                                      kernel_regularizer = self.regularizer))
         #Optimizers
@@ -57,10 +57,10 @@ class ClassifierNN:
         model.compile(loss='mean_squared_error',
                       optimizer=opt,
                       metrics=['accuracy'])
-        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=15, verbose=0, mode='auto', baseline=None)
+        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=20, verbose=0, mode='auto', baseline=None)
         model.fit(self.data_training, self.label_training,
                   epochs=2000,
-                  batch_size=50,
+                  batch_size=10,
                   callbacks=[early_stopping],
                   shuffle=True)
 
