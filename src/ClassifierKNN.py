940fe84087eb13448428a559665ce9b3138edd1f
Shichao Ma <hzmashichao@163.com>
2019-03-23 22:03:22 +0000

    Update ClassifierKNN.py

--- src/ClassifierKNN.py
+++ src/ClassifierKNN.py
@@ -6,6 +6,17 @@ import os
 from pathlib import Path
 import Utility
 
+
+import random as rn
+To avoid the randomness
+import os
+os.environ['PYTHONHASHSEED']=str(1)
+np.random.seed(1)
+rn.seed(1)
+
+
+
+
 class ClassifierKNN:
 
     data_training = []
