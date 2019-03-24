c98a9a90409adb57557cf53baff96b5687832298
Shichao Ma <hzmashichao@163.com>
2019-03-24 00:41:09 +0000

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -54,10 +54,10 @@ class ClassifierNN:
         model = keras.models.Sequential()
         #The layers of NN
         model.add(keras.layers.Dense(180, activation='relu', input_dim=self.input_size))
-        #model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dropout(0.1))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.Dense(180, activation='relu'))
-        #model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dropout(0.1))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.Dense(180, activation='relu'))
         model.add(keras.layers.Dropout(0.3))
@@ -67,7 +67,7 @@ class ClassifierNN:
         model.compile(loss='mean_squared_error',
                       optimizer=opt,
                       metrics=['accuracy'])
-        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=3, verbose=0, mode='auto', baseline=None)
+        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=8, verbose=0, mode='auto', baseline=None)
         model.fit(self.data_training, self.label_training,
                   epochs=2000,
                   batch_size=10,
