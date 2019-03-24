917310cc139f3a4a0ee545e94c4ef08c79cbd9bb
Shichao Ma <hzmashichao@163.com>
2019-03-24 13:26:59 +0000

    Update ClassifierCNN.py

--- src/ClassifierCNN.py
+++ src/ClassifierCNN.py
@@ -50,7 +50,6 @@ class ClassifierCNN:
         model.add(keras.layers.Dropout(0.5))
         model.add(keras.layers.Flatten())
         model.add(keras.layers.Dense(200, activation='tanh', kernel_regularizer=self.regularizer, activity_regularizer=self.regularizer))
-        model.add(keras.layers.Dropout(0.2))
         model.add(keras.layers.Dense(150, activation='tanh', kernel_regularizer=self.regularizer, activity_regularizer=self.regularizer))
         model.add(keras.layers.Dropout(0.5))
         model.add(keras.layers.Dense(15, activation=tf.nn.softmax))
