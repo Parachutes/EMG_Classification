47d8decc0459e4eb5951c7a025a8d44c435830e3
Shichao Ma <hzmashichao@163.com>
2019-03-29 23:40:53 +0000

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -60,7 +60,7 @@ class ClassifierNN:
         model.add(keras.layers.Dense(180, activation='relu'))
         model.add(keras.layers.Dropout(0.4))
         model.add(keras.layers.Dense(180, activation='relu'))
-        model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dropout(0.5))
         model.add(keras.layers.Dense(15, activation=tf.nn.softmax))
         #Optimizers
         opt = keras.optimizers.Adam(lr=0.0005)
