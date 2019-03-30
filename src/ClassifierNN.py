5fe567e7370d20c97a3e2f08aabc665eb4a52790
Shichao Ma <hzmashichao@163.com>
2019-03-30 13:23:20 +0000

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -71,7 +71,7 @@ class ClassifierNN:
         early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=30, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
         model.fit(self.data_training, self.label_training,
                   epochs=2000,
-                  batch_size=20,
+                  batch_size=32,
                   callbacks=[early_stopping],
                   shuffle=True)
         
