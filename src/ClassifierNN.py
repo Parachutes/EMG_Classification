cbab6e3e2232161bf1fe7f985f58ef4edd7e56fa
Shichao Ma <hzmashichao@163.com>
2019-03-30 21:21:33 +0000

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -63,7 +63,7 @@ class ClassifierNN:
         #model.add(keras.layers.Dropout(0.3))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.Dense(225, activation='relu', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
-        model.add(keras.layers.Dropout(0.4))
+        model.add(keras.layers.Dropout(0.5))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.Dense(15, activation=tf.nn.softmax))
         
@@ -74,7 +74,7 @@ class ClassifierNN:
         model.compile(loss='mean_squared_error',
                       optimizer=opt,
                       metrics=['accuracy'])
-        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=10, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
+        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=40, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
         model.fit(self.data_training, self.label_training,
                   epochs=2000,
                   batch_size=200,
