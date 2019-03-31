ff03bfa2328d254368f7c4bb10b98a2137e01d59
Shichao Ma <hzmashichao@163.com>
2019-03-31 16:55:44 +0100

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -57,16 +57,10 @@ class ClassifierNN:
         model = keras.models.Sequential()
         #The layers of NN
         model.add(keras.layers.Dense(225, activation='relu', input_dim=self.input_size, kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
-        #model.add(keras.layers.Dropout(0.1))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.Dense(225, activation='relu', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
         model.add(keras.layers.Dropout(0.1))
         model.add(keras.layers.BatchNormalization())
-        
-        model.add(keras.layers.Dense(225, activation='relu', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
-        model.add(keras.layers.Dropout(0.2))
-        model.add(keras.layers.BatchNormalization())
-        
         model.add(keras.layers.Dense(225, activation='relu', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
         model.add(keras.layers.Dropout(0.6))
         model.add(keras.layers.BatchNormalization())
