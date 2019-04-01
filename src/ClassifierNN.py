500ab47a0e5e9b63c0c0493439ceb1bbaf380959
Shichao Ma <hzmashichao@163.com>
2019-04-01 16:51:14 +0100

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -37,7 +37,7 @@ class ClassifierNN:
     input_size = 0
 
     #regularizer = keras.regularizers.l2(l=0.000075)
-    regularizer = keras.regularizers.l2(l=0.002)
+    regularizer = keras.regularizers.l2(l=0.003)
 
     #The constructor
     def __init__(self, data_training, label_training, data_testing):
