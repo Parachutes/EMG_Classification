7eac343abf87a153f1eb3e47ab1138f67bd30b38
Shichao Ma <hzmashichao@163.com>
2019-04-01 18:52:28 +0100

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -74,10 +74,10 @@ class ClassifierNN:
         model.compile(loss='mean_squared_error',
                       optimizer=opt,
                       metrics=['accuracy'])
-        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=50, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
+        early_stopping = keras.callbacks.EarlyStopping(monitor='acc', patience=150, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
         model.fit(self.data_training, self.label_training,
                   epochs=3000,
-                  batch_size=150,
+                  batch_size=1500,
                   callbacks=[early_stopping],
                   shuffle=True)
         
@@ -116,7 +116,7 @@ Utility.collect_testing_data_with_windowing(path_dataset, x_test, y_test, ["S1",
 
 result_list = []
 
-for i in range(10):
+for i in range(5):
     print("attempt: ", i+1)
     classifierNN = ClassifierNN(x_train, y_train, x_test)
     classifierNN.train_the_model()
