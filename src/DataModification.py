f8a8033d85aa3d668e5f2b8716094d062ad37ce1
Shichao Ma <hzmashichao@163.com>
2019-04-02 12:08:47 +0100

    Update DataModification.py

--- src/DataModification.py
+++ src/DataModification.py
@@ -94,7 +94,8 @@ def write_raw_windowing(crop_size, window_size, interval):
             original_matrix = Utility.read_csv(f)
             
             #Do the normalization
-            original_matrix_norm = keras.utils.normalize(original_matrix)
+            #original_matrix_norm = keras.utils.normalize(original_matrix)
+            original_matrix_norm = original_matrix
             
             rotated_matrix = rotate_raw_data(original_matrix_norm)
             cropped_matrix = crop_data(rotated_matrix, crop_size)
