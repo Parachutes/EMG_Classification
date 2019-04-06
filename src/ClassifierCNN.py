a0a09bccced4ac3339c5eea5f75a41c369aa301e
Shichao Ma <hzmashichao@163.com>
2019-04-06 21:13:52 +0100

    Update ClassifierCNN.py

--- src/ClassifierCNN.py
+++ src/ClassifierCNN.py
@@ -1,4 +1,3 @@
-#DONE
 #Shichao.Ma$$$IndividualProject
 #This is the Conventional Neural Network
 import numpy as np
@@ -29,27 +28,18 @@ class ClassifierCNN:
     data_training = []
     label_training = []
     data_testing = []
-    
     predictions = []
     
     regularizer = keras.regularizers.l2(l=0.00012)
     
-  
-    
-    
     def __init__(self, data_training, label_training, data_testing):
         
-        #Tricks in "reshape"
-        
+        #Tricks in "reshape", for full details please check the third chapter in the report
         self.data_training = np.array(data_training).reshape(len(data_training),4000,8,1)
         self.label_training = [Utility.label_str2array(l) for l in label_training]
         self.label_training = np.array(self.label_training).reshape(len(label_training), 15)  
         self.data_testing = data_testing
         
-    
-    
-    
-    
     def train_the_model(self):
         model = keras.models.Sequential()
         model.add(keras.layers.Conv2D(filters=5, kernel_size=(50,8), activation='tanh', input_shape=(4000,8,1), padding='same', kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer))
@@ -87,18 +77,12 @@ class ClassifierCNN:
             self.predictions.append(max(set(prediction), key=prediction.count))
             
 
-
-
     #To get the prediction through the model
     def get_predictions(self):
         return self.predictions
-    
-
-
-
-
-
-#Script part
+   
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Executable part
 current_path = os.path.abspath(os.path.dirname(__file__))
 current_path_parent = str(Path(current_path).parent)
 path_dataset = current_path_parent + '/data/raw_windowing'
@@ -109,10 +93,8 @@ y_train = []
 x_test = []
 y_test = []
 
-Utility.collect_data_with_windowing(path_dataset, x_train, y_train, ["S1", "S2", "S3","S4", "S5", "S6","S7", "S8"], ["1","3"])
-Utility.collect_testing_data_with_windowing(path_dataset, x_test, y_test, ["S1", "S2", "S3","S4", "S5", "S6","S7", "S8"], ["2"])
-
-
+Utility.collect_data_with_windowing(path_dataset, x_train, y_train, ["S1", "S2", "S3","S4", "S5", "S6","S7", "S8"], ["1","2"])
+Utility.collect_testing_data_with_windowing(path_dataset, x_test, y_test, ["S1", "S2", "S3","S4", "S5", "S6","S7", "S8"], ["3"])
 
 
 classifierCNN = ClassifierCNN(x_train, y_train, x_test)
