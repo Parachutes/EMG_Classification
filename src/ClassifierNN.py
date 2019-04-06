e905c8b5f540ae9af3f7456239d536f368f34273
Shichao Ma <hzmashichao@163.com>
2019-04-06 21:12:21 +0100

    Update ClassifierNN.py

--- src/ClassifierNN.py
+++ src/ClassifierNN.py
@@ -1,7 +1,5 @@
-#DONE
 #Shichao.Ma$$$IndividualProject
 #This is the Traditional Neural Network Classifier
-
 
 import numpy as np
 import tensorflow as tf
@@ -25,8 +23,6 @@ sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
 K.set_session(sess)
 
 
-
-
 class ClassifierNN:
 
     #Fields
@@ -36,22 +32,17 @@ class ClassifierNN:
     predictions = []
     input_size = 0
 
-    #regularizer = keras.regularizers.l2(l=0.000075)
     regularizer = keras.regularizers.l2(l=0.0015)
 
     #The constructor
     def __init__(self, data_training, label_training, data_testing):
-
         self.predictions = []
-        
         self.data_training = [d.flatten() for d in data_training]
         self.input_size = len(self.data_training[0])
         self.data_training = np.array(self.data_training).reshape(len(data_training), self.input_size)
         self.label_training = [Utility.label_str2array(l) for l in label_training]
         self.label_training = np.array(self.label_training).reshape(len(label_training), 15)
         self.data_testing = data_testing
-        
-
 
     #This method is to train the nn model
     def train_the_model(self):
@@ -68,7 +59,6 @@ class ClassifierNN:
         model.add(keras.layers.Dense(15, activation=tf.nn.softmax))
         
         #Optimizers
-        #opt = keras.optimizers.Adam(lr=0.0005, decay=1e-6)
         opt = keras.optimizers.Adagrad(lr=0.0005, epsilon=None, decay=0)
         
         model.compile(loss='mean_squared_error',
@@ -81,8 +71,6 @@ class ClassifierNN:
                   callbacks=[early_stopping],
                   shuffle=True)
         
-
-
         # Do the prediction
         for d_t in self.data_testing:
             d_t = [d.flatten() for d in d_t]
@@ -91,15 +79,13 @@ class ClassifierNN:
             prediction = [Utility.label_num2str(np.argmax(p)) for p in prediction]
             self.predictions.append(max(set(prediction), key=prediction.count))
 
-
-
     #To get the prediction through the model
     def get_predictions(self):
         return self.predictions
 
 
-
-#Script part
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Executable part
 current_path = os.path.abspath(os.path.dirname(__file__))
 current_path_parent = str(Path(current_path).parent)
 path_dataset = current_path_parent + '/data/features_windowing'
@@ -114,8 +100,6 @@ Utility.collect_data_with_windowing(path_dataset, x_train, y_train, ["S1", "S2",
 Utility.collect_testing_data_with_windowing(path_dataset, x_test, y_test, ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"], ["3"])
 
 
-
-    
 classifierNN = ClassifierNN(x_train, y_train, x_test)
 classifierNN.train_the_model()
 result = Utility.get_accuracy(classifierNN.get_predictions(), y_test)    
